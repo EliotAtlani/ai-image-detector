18d9852d2b5c73e3b2b1ca96f6fc2bae316bcd62
mathilde-cros <mathilde.cros02@gmail.com>
2025-04-03 22:55:41 -0400

    Faces resize

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 # Global configuration parameters
 IMG_SIZE = (250, 250)
 DATA_DIR = "../data/art"
-TFRECORDS_FILE = "dataset.tfrecords"
+# TFRECORDS_FILE = "dataset.tfrecords"
 OUTPUT_DIR = "../data_resized/art"
-OUTPUT_TFRECORD_DIR = "../data_processed/art"
+# OUTPUT_TFRECORD_DIR = "../data_processed/art"
 # Derived paths for class-specific directories
 FAKE_DIR = os.path.join(DATA_DIR, "fake")
 REAL_DIR = os.path.join(DATA_DIR, "real")
